9a895510b3389c413790968ca2ae28b6a1ba8b21
macielexpedito <macielexpedito@gmail.com>
2026-03-02 19:56:13 -0300

    desafio

---
+++ 6 - consumo.js
@@ -0,0 +1,34 @@
+// --- Calculadora de Consumo de Combustível ---
+
+const precos = {
+  gasolina: 5.8,
+  alcool: 3.9,
+  diesel: 6.20
+};
+
+function calcularGasto(distancia, consumoKml, tipoCombustivel) {
+  let precoUnitario = tipoCombustivel;
+
+  let litrosNecessarios = distancia / consumoKml;
+  let custoTotal = litrosNecessarios * precoUnitario;
+
+  return custoTotal;
+}
+
+const viagem = {
+  destino: "Litoral",
+  distancia: "200",
+  veiculo: "Corsa",
+};
+
+const resultadoGasolina = calcularGasto(viagem.distancia, 10, precos.gasolina);
+
+console.log("Viagem para: " + viagem.destino);
+console.log("Custo estimado no Diesel: R$ " + calcularGasto(200, 15, precos.diesel));
+console.log("Custo estimado na Gasolina: R$ " + custoTotal);
+
+if (precos.alcool < 4) {
+  const mensagem = "O álcool está compensando!";
+}
+
+console.log(mensagem);
